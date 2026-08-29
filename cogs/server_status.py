2ea6339f74dc5a3326046b54c6823e4d68a00ab3
Yure Thalles <yurethalles@hotmail.com>
2026-08-28 22:22:51 -0300

    Bioma no env

--- cogs/server_status.py
+++ cogs/server_status.py
@@ -1,9 +1,13 @@
 import asyncio
+import os
+
 import a2s
 import discord
+from dotenv import load_dotenv
 
 from discord.ext import commands, tasks
 
+load_dotenv()
 
 SERVER_IP = "177.54.147.114"
 QUERY_PORT = 24667
@@ -15,7 +19,7 @@ CHANNEL_ID = 1543025024645210303
 # Se deixar 0, o bot cria uma nova e mostra o ID no terminal
 MESSAGE_ID = 1543039966890823694
 
-WORLD_BIOME = "Mistlands"
+WORLD_BIOME = os.getenv("WORLD_BIOME", "Mistlands")
 
 class ServerStatus(commands.Cog):
 
